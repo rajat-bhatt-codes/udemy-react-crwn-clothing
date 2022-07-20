811573b124545df8bc0e9f78284c2d4d7bc38e36
Rajat Bhatt <rajatbhatt500@gmail.com>
2022-07-20 11:48:36 +0530

    redux saga introduced

--- src/store/store.js
+++ src/store/store.js
@@ -3,20 +3,25 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
-import thunk from "redux-thunk";
+//import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
-//whitelistb means the reducer part we want to persist
+import { rootSaga } from "./root-saga";
+
+//whitelist means the reducer part we want to persist
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -32,5 +37,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
